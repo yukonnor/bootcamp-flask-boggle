e1428b9069ef4ea75dbdc546483a9b7fa9bf8864
connor <caschm@gmail.com>
2023-12-18 15:48:28 -0500

    add base logic to submit and check word guess

--- app.py
+++ app.py
@@ -13,10 +13,22 @@ print(board)
 
 @app.route('/')
 def index():
-    """Show boggle board."""
+    """Show boggle board and word guess form."""
 
     # Store board in the session
     session['board'] = board
+
+    return render_template("board.html", board=board)
+
+@app.route('/check-word')
+def index():
+    """Check if word is valid"""
+
+    # guess guess from query string
+    word_guess = request.args["word-guess"]
+
+    # check guess: "ok", "not-on-board", "not-word"
+    result = boggle_game.check_valid_word(board, word_guess)
 
     return render_template("board.html", board=board)
 
