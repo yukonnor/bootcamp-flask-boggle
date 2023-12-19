b2786ab558b29d388a60a05a5f5511ce62dadde4
connor <caschm@gmail.com>
2023-12-18 19:17:53 -0500

    add bootstrap and improve styling of alerts

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
+// get elements we'll be working with
 $form = $("form");
 $wordInput = $("#word-guess");
+$resultContainer = $("#result");
 
 // make a request to the server to see if the word is valid
 async function check_word(word) {
@@ -9,6 +11,26 @@ async function check_word(word) {
     console.log(`check_word() response.data: ${JSON.stringify(response.data)}`);
 
     return response.data["result"];
+}
+
+function show_result(result, word) {
+    // depending on the result, update the class and text of the container
+
+    // Remove all alert classes so that we can add the class that we're updating to
+    $resultContainer.removeClass("alert-success alert-danger alert-warning alert-info");
+
+    word = word.toUpperCase();
+
+    if (result === "ok") {
+        $resultContainer.html(`Count it! <b>${word}</b> is in the board`);
+        $resultContainer.addClass("alert-success");
+    } else if (result === "not-on-board") {
+        $resultContainer.html(`Nice try! <b>${word}</b> isn't on the board`);
+        $resultContainer.addClass("alert-danger");
+    } else if (result === "not-word") {
+        $resultContainer.html(`Sorry. <b>${word}</b> isn't in our dictionary`);
+        $resultContainer.addClass("alert-danger");
+    }
 }
 
 async function process_word_submission(event) {
